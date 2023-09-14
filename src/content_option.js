3719e5afd722793dce3c6e55c96bdb35ab5e0f89
T E B O O <tjaynthako@gmail.com>
2023-09-14 10:48:24 +0200

    updates

---
+++ src/content_option.js
@@ -0,0 +1,127 @@
+const logotext = "TEBOHO NTHAKO";
+const meta = {
+    title: "Teboho Nthako",
+    description: "React Front-end developer_ and I love to craft solid & scalable software with great user-experiences.",
+};
+
+const introdata = {
+    title: "I’m Teboho Nthako",
+    animated: {
+        first: "I'm a React Front-end Developer",
+        second: "JavaScript enthusiast",
+        third: "I code cool websites",
+    },
+    description: "I love combining the worlds of logic and creative design to make beautiful, accessible and user-friendly websites and applications. I value myself on being a great contribution to any team.",
+    your_img_url: "https://media.licdn.com/dms/image/D4D03AQGmBbrdDGauBA/profile-displayphoto-shrink_800_800/0/1693217354677?e=2147483647&v=beta&t=3UfcRkiNzZkfN-j7zQh_hEA0dgMQ6sJLZuhD4g5wvgQ",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "Hello World! My name is Teboho Nthako, A Front end developer based in Johannesburg. I'm a Biological science Honors graduate who fell in love with code and the problem-solving of it all. I love to craft solid & scalable software with great user-experiences. In my spare time you'll catch me trying out new recipes from pinterest, creating fashion content for my YT channel or working on my personal projects that expose me to new programming languages and technologies. I believe people who make tech should be predominantly connected to the problems we face everyday.",
+};
+const worktimeline = [{
+        jobtitle: "Technical mentor & Junior Software engineer intern",
+        where: "Melsoft",
+        date: "May 2023 - Present",
+    },
+    
+];
+
+const skills = [{
+        name: "HTML & CSS",
+        value: 95,
+    },
+    {
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Git",
+        value: 90,
+    },
+    {
+        name: "Netlify",
+        value: 90,
+    },
+    {
+        name: "GitHub",
+        value: 90,
+    },
+    {
+        name: "Bootstrap",
+        value: 95,
+    },
+    {
+        name: "Node",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Software development / Web development",
+        description: "Looking to work on web applications and mobile apps that help make people's lives alot easier.",
+    },
+    
+];
+
+const dataportfolio = [
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Apple store clone ecommerce site. Collaborated with fellow Software engineer Saajida Mathen",
+        link: "https://teboo-saaj-applestore.netlify.app/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Ticketstation",
+        link: "https://tebohonthako.github.io/ticket-station-added-the-ticket-form/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "MovieLand react-app.",
+        link: "https://teboos-movieland.netlify.app/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "My Portfolio of work as a Developer using React",
+        link: "https://teboho-nthako-portfolio.netlify.app/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "iOS Calculator",
+        link: "https://tebohonthako.github.io/my-calculator/",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "tjaynthako@gmail.com",
+    YOUR_FONE: "(+27) 7644-34084",
+    description: "Let's connect.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_crt4r4b",
+    YOUR_TEMPLATE_ID: "template_jvcu046",
+    YOUR_USER_ID: "DKPUublal98hza8Ko",
+};
+
+const socialprofils = {
+    github:   "https://github.com/tebohonthako",
+    linkedin: "https://www.linkedin.com/in/tebohonthako/",
+    twitter:  "https://twitter.com/tebooo_n",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
